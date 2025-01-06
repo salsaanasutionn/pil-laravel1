1cb89c48b232e470bcca5ad6a13b1293b2dd4e10
salsaanasutionn <fnasution170@gmail.com>
2025-01-06 16:46:03 +0700

    project pbwl

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('profile', function () {
+    $nama = 'Salsa';
+    return view('profile', compact('nama'));
+});
